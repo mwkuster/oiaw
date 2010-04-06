ef59ccb10d81ee77f4a2293f8732e0cdf3a15b1c
mwkuster <mwkuster@93252124-db06-59e3-c53f-f19a3f6f2177>
2010-04-06 16:57:17 +0000

    code reorganization + partion method added

---
+++ split.scala
@@ -0,0 +1,19 @@
+package eu.budabe.oiaw
+
+object Utility {
+def splitBefore(lst : List[String], splitAt: String => Boolean) : List[List[String]] =  {
+  def split_rec(lst : List[String], curr_lst : List[String], res : List[List[String]]) : List[List[String]] = {
+    lst match {
+      case x :: xs if splitAt(x) => {
+        split_rec(xs, x :: List(), (curr_lst.reverse) :: res)
+      }
+      case x :: xs => {
+        split_rec(xs, x :: curr_lst, res)
+      }
+      case Nil =>  (curr_lst.reverse :: res).reverse
+    }
+  }
+  
+  split_rec(lst, List(), List())
+}
+}
